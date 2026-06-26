c32b93f76a2486afe96a1ce832b44ff2807a7af4
xiaodaoaa <xiaodaoaa@gmail.com>
2026-06-26 15:21:31 +0800

    feat: add Electron main process with IPC and preload

---
+++ EHttpServerTool/electron/main.js
@@ -0,0 +1,113 @@
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+const path = require('path')
+const { PathConfigManager } = require('./modules/path-config')
+const { RequestLogger } = require('./modules/request-logger')
+const { HttpServerManager } = require('./modules/http-server')
+
+let mainWindow = null
+let pathConfigManager = null
+let requestLogger = null
+let httpServerManager = null
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  })
+
+  const devUrl = process.env.VITE_DEV_SERVER_URL
+  if (devUrl) {
+    mainWindow.loadURL(devUrl)
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '..', 'dist', 'index.html'))
+  }
+}
+
+function sendToRenderer(channel, data) {
+  if (mainWindow && !mainWindow.isDestroyed()) {
+    mainWindow.webContents.send(channel, data)
+  }
+}
+
+function setupModules() {
+  const userDataPath = app.getPath('userData')
+  pathConfigManager = new PathConfigManager(userDataPath)
+  requestLogger = new RequestLogger()
+  httpServerManager = new HttpServerManager(pathConfigManager, requestLogger)
+}
+
+function setupIPC() {
+  // Server control
+  ipcMain.handle('start-server', async (_event, port) => {
+    return httpServerManager.start(port, sendToRenderer)
+  })
+
+  ipcMain.handle('stop-server', async () => {
+    return httpServerManager.stop()
+  })
+
+  // Path config
+  ipcMain.handle('add-path', async (_event, config) => {
+    return pathConfigManager.add(config)
+  })
+
+  ipcMain.handle('update-path', async (_event, config) => {
+    return pathConfigManager.update(config)
+  })
+
+  ipcMain.handle('delete-path', async (_event, id) => {
+    return pathConfigManager.remove(id)
+  })
+
+  ipcMain.handle('list-paths', async () => {
+    return pathConfigManager.getAll()
+  })
+
+  // Logs
+  ipcMain.handle('get-logs', async () => {
+    return requestLogger.getAll()
+  })
+
+  ipcMain.handle('clear-logs', async () => {
+    return requestLogger.clear()
+  })
+
+  // Context menu
+  ipcMain.handle('show-context-menu', async () => {
+    return new Promise((resolve) => {
+      const menu = Menu.buildFromTemplate([
+        {
+          label: '清空',
+          click: () => resolve('clear'),
+        },
+      ])
+      menu.popup({
+        callback: () => resolve(null),
+      })
+    })
+  })
+}
+
+app.whenReady().then(() => {
+  setupModules()
+  setupIPC()
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (httpServerManager) {
+    httpServerManager.stop()
+  }
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
---
+++ EHttpServerTool/electron/preload.js
@@ -0,0 +1,44 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Server control
+  startServer: (port) => ipcRenderer.invoke('start-server', port),
+  stopServer: () => ipcRenderer.invoke('stop-server'),
+
+  // Path config
+  addPath: (config) => ipcRenderer.invoke('add-path', config),
+  updatePath: (config) => ipcRenderer.invoke('update-path', config),
+  deletePath: (id) => ipcRenderer.invoke('delete-path', id),
+  listPaths: () => ipcRenderer.invoke('list-paths'),
+
+  // Logs
+  getLogs: () => ipcRenderer.invoke('get-logs'),
+  clearLogs: () => ipcRenderer.invoke('clear-logs'),
+
+  // Context menu
+  showContextMenu: () => ipcRenderer.invoke('show-context-menu'),
+
+  // Event listeners
+  onServerStarted: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('server-started', handler)
+    return () => ipcRenderer.removeListener('server-started', handler)
+  },
+  onServerStopped: (callback) => {
+    const handler = () => callback()
+    ipcRenderer.on('server-stopped', handler)
+    return () => ipcRenderer.removeListener('server-stopped', handler)
+  },
+  onServerError: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('server-error', handler)
+    return () => ipcRenderer.removeListener('server-error', handler)
+  },
+  onNewRequest: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('new-request', handler)
+    return () => ipcRenderer.removeListener('new-request', handler)
+  },
+
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
+})
