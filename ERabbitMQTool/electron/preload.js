2475fb94ff23f4677a9445f28c430d4138b555ca
xiaodaoaa <xiaodaoaa@gmail.com>
2026-07-14 16:52:42 +0800

    feat: add full preload API

--- ERabbitMQTool/electron/preload.js
+++ ERabbitMQTool/electron/preload.js
@@ -1,5 +1,39 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  connect: (config) => ipcRenderer.invoke('connect', config),
+  disconnect: () => ipcRenderer.invoke('disconnect'),
+  publish: (target) => ipcRenderer.invoke('publish', target),
+  subscribe: (queue) => ipcRenderer.invoke('subscribe', queue),
+  unsubscribe: (consumerTag) => ipcRenderer.invoke('unsubscribe', consumerTag),
+  saveConfig: (config) => ipcRenderer.invoke('save-config', config),
+  loadConfig: () => ipcRenderer.invoke('load-config'),
+
+  onConnected: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('connected', handler)
+    return () => ipcRenderer.removeListener('connected', handler)
+  },
+  onDisconnected: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('disconnected', handler)
+    return () => ipcRenderer.removeListener('disconnected', handler)
+  },
+  onConnectionError: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('connection-error', handler)
+    return () => ipcRenderer.removeListener('connection-error', handler)
+  },
+  onMessageReceived: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('message-received', handler)
+    return () => ipcRenderer.removeListener('message-received', handler)
+  },
+  onPublishConfirmed: (callback) => {
+    const handler = (_event, data) => callback(data)
+    ipcRenderer.on('publish-confirmed', handler)
+    return () => ipcRenderer.removeListener('publish-confirmed', handler)
+  },
+
   removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
 })
